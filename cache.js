548c7fdd57728fdb5b7b58d7d7bbcd2afd389900
Johny Jugianto <johny.jugianto@gmail.com>
2019-10-29 07:47:58 +0700

    Add standalonce cache

--- cache.js
+++ cache.js
@@ -1,57 +1,114 @@
-const Fs      = require('fs');
-const Cluster = require('cluster');
-const Memored = require('memored');
+const NodeCache = require("node-cache");
 
-const TIMEOUT = (process.env.CACHE_TIMEOUT) ? process.env.CACHE_TIMEOUT : (5 * 60 * 1000);
+class ClusterCache {
 
-function init(beforePurgeFn)
-{
-  Memored.setup({
-    purgeInterval: TIMEOUT,
-    beforePurge: function(entry) {
-      if (beforePurgeFn) {
-        return beforePurgeFn(entry);
-      } else {
-        return true;
+  init(beforePurgeFn = null) {
+    Memored.setup({
+      purgeInterval: TIMEOUT,
+      beforePurge: function(entry) {
+        if (beforePurgeFn) {
+          return beforePurgeFn(entry);
+        } else {
+          return true;
+        }
       }
+    });
+  }
+
+  get(key, callback) {
+    Memored.read(key, callback);
+  }
+
+  set(key, value, timeout, callback) {
+    Memored.store(key, value, timeout, callback);
+  }
+
+  setForever(key, value, callback) {
+    Memored.store(key, value, callback);
+  }
+
+  del(key, callback) {
+    Memored.remove(key, callback);
+  }
+}
+
+class StandaloneCache {
+
+  constructor() {
+    this.nCache = new NodeCache();
+  }
+
+  init(beforePurgeFn = null) {
+    this.nCache.on("expired", function(key, value) {
+      return beforePurgeFn(value);
+    });
+  }
+
+  get(key, callback) {
+    return callback(null, this.nCache.get(key));
+  }
+
+  set(key, value, timeout, callback) {
+    var success = this.nCache.set(key, value, timeout);
+    this.handleSetResponse(success, callback);
+  }
+
+  handleSetResponse(success, callback) {
+    if (success) {
+      return callback(null);
+    } else {
+      return callback({ message: 'Set cache failed' });
     }
-  });
+  }
+
+  setForever(key, value, callback) {
+    var success = this.nCache.set(key, value);
+    this.handleSetResponse(success, callback);
+  }
+
+  del(key, callback) {
+    this.nCache.del(key);
+    return callback(null);
+  }
 }
 
-function get(key, callback)
-{
-  Memored.read(key, callback);
+var instance = null;
+
+function init(beforePurgeFn = null, isStandalone = false) {
+  instance = (isStandalone) ? new StandaloneCache() : new ClusterCache();
+  instance.init(beforePurgeFn);
 }
 
-function set()
-{
-  var key   = arguments[0];
+function get(key, callback) {
+  instance.get(key, callback);
+}
+
+function set() {
+  var key = arguments[0];
   var value = arguments[1];
   var timeout, callback;
   if (arguments.length > 3) {
-    timeout  = (arguments[2]) ? arguments[2] : TIMEOUT;
+    timeout = (arguments[2]) ? arguments[2] : TIMEOUT;
     callback = arguments[3];
   } else {
-    timeout  = TIMEOUT;
+    timeout = TIMEOUT;
     callback = arguments[2];
   }
-  Memored.store(key, value, timeout, callback);
+  instance.set(key, value, timeout, callback);
 }
 
-function setForever(key, value, callback)
-{
-  Memored.store(key, value, callback);
+function setForever(key, value, callback) {
+  instance.setForever(key, value, callback);
 }
 
-function del(key, callback)
-{
-  Memored.remove(key, callback);
+function del(key, callback) {
+  instance.del(key, callback);
 }
 
 module.exports = {
-  init       : init,
-  get        : get,
-  set        : set,
-  setForever : setForever,
-  del        : del
+  init: init,
+  get: get,
+  set: set,
+  setForever: setForever,
+  del: del
 }
